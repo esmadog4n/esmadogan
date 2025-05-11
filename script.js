a73838bf5b6822a8a25dfcc81beb169266fb445c
esma <136924655+esmadog4n@users.noreply.github.com>
2025-05-11 22:15:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("aramaForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Sayfa yenilenmesin
+  
+    const arama = document.getElementById("aramaKutusu").value.toLowerCase();
+  
+    // Anahtar kelimelere göre yönlendirme listesi
+    const yönlendirmeListesi = {
+      "glütensiz": ["pkekgz.html"],
+      "kek": ["tarifaramaliste.html"], // Artık tüm kek tariflerini bu sayfada listeleyeceğiz
+      "kahvaltılık": ["kaygana.html"]
+    };
+  
+    let bulunanSayfalar = [];
+  
+    for (const anahtarKelime in yönlendirmeListesi) {
+      if (arama.includes(anahtarKelime)) {
+        bulunanSayfalar = [...bulunanSayfalar, ...yönlendirmeListesi[anahtarKelime]];
+      }
+    }
+  
+    const benzersizSayfalar = [...new Set(bulunanSayfalar)];
+  
+    if (benzersizSayfalar.length === 1) {
+      window.location.href = benzersizSayfalar[0];
+    } else if (benzersizSayfalar.length > 1) {
+      // Burada yönlendirme yapılacak sayfaya gidiyoruz
+      window.location.href = "tarifler.html";  // Bu sayfa tüm kek tariflerini listeleyecek
+    } else {
+      alert("Aradığınız tarif bulunamadı.");
+    }
+  });
+  
